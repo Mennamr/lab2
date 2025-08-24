bb080946b426ba7788f70ff145ad441428ed967f
mennamr <mennamrahmed@gmail.com>
2025-08-24 15:09:28 +0300

    add HelloITI class

---
+++ HelloITI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+    {
+        internal class HelloITI
+        {
+            public static void printHelloITI()
+            {
+                Console.WriteLine("Hello ITI");
+
+            }
+            static void Main(string[] args)
+            {
+                printHelloITI();
+
+            }
+        }
+    }
