96c6bbb1e6b8d3d5160aa7db75dfef88886cad39
mennamr <mennamrahmed@gmail.com>
2025-08-24 15:09:11 +0300

    add helloWorld class

---
+++ HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void printHelloWorld()
+        {
+            Console.WriteLine("Hello world");
+
+        }
+        static void Main(string[] args)
+        {
+            printHelloWorld();
+
+        }
+    }
+}
