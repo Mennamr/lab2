a9529904b9bc20314f0e9575a17a98df1ca67680
mennamr <mennamrahmed@gmail.com>
2025-08-25 16:25:53 +0300

    added logine in helloITI file

--- HelloITI.cs
+++ HelloITI.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//menna edited this cs file again
+
 namespace HelloWorld
     {
-        internal class HelloITI
+    //hi this is logine and menna
+    internal class HelloITI
         {
             public static void printHelloITI()
             {
